9cebde1c0d91db2305c6077dd3a47f0156a9f171
abhinav-8 <abhinavkumar.ak9@gmail.com>
2023-03-12 02:57:12 +0530

    Second day , 1 module done

--- hello.js
+++
@@ -1,2 +0,0 @@
-let x = 059 ;
-console.log(x);
---
+++ jsIntro/function.js
@@ -0,0 +1,13 @@
+//You can assign a function to a variable
+//In c++ we can do the same using function pointer
+function add(x,y){
+    return x+y;
+}
+let result = add;
+console.log(result(2,3));
+
+//Output -> 5
+
+//Function return undefined when we don't return anything
+
+
