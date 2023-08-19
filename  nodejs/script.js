76334522005d142d20582d2e5d6012d55e2c867d
abhinav-8 <abhinavkumar.ak9@gmail.com>
2023-08-20 00:11:09 +0530

    added script.js

---
+++  nodejs/script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+
+// How to write scripts in node
+//Run like ./script.js abhinav nodejs
+//Error? Write chmod 777 script.js 
+console.log("Hello world");
+console.log(process.argv);
