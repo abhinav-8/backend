f66a29429be3f45adc59c7fd3e4672d22092b02b
abhinav-8 <abhinavkumar.ak9@gmail.com>
2023-08-20 20:52:04 +0530

    changed output stream to stdout

---  nodejs/streamDemo/streamDemo.js
+++  nodejs/streamDemo/streamDemo.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const transformStream = require('stream');
 
 let fileStream=fs.createReadStream(__dirname + "/input.txt");
-// let outputStream=process.stdout;
+let outputStream=process.stdout;
 // let outputStream = fs.createWriteStream(__dirname + '/output.txt', {
 //     flags: 'w'
 // });
@@ -14,7 +14,7 @@ let middleStream = new transformStream.Transform({
         let modifiedChunk = chunk.toString().toUpperCase();
         this.push(modifiedChunk);
         setTimeout(nextCB,1000); //To give a feel of delay while reading and writing
-        // nextCB();
+        // nextCB();        
     }
 });
 
