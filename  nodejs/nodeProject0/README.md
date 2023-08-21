2c76f7ea63dcef5e6002435cefafa5979ff0ceb4
abhinav-8 <abhinavkumar.ak9@gmail.com>
2023-08-21 17:48:37 +0530

    added readme

---
+++  nodejs/nodeProject0/README.md
@@ -0,0 +1,10 @@
+## Shady Lyrics
+
+It is a telegram chatbot made in nodejs which gives you the lyrics of the songs by Eminem.
+
+Packages used: 
+- Telegraph
+- genius lyrics api
+
+
+This is my first node js handson.
---  nodejs/nodeProject0/index.js
+++  nodejs/nodeProject0/index.js
@@ -1,6 +1,6 @@
-const {Telegraf} = require('telegraf');
-const { getLyrics} = require('genius-lyrics-api');
-require('dotenv').config()
+const { Telegraf } = require("telegraf");
+const { getLyrics } = require("genius-lyrics-api");
+require("dotenv").config();
 /**
  * Steps to get secret token for creating a bot
  * -> Open telegram and search for BotFather
@@ -13,28 +13,25 @@ require('dotenv').config()
 
 const bot = new Telegraf(process.env.TELEGRAM_API_KEY);
 
-bot.start((ctx) => ctx.reply('Welcome to shady lyrics stan'))
+bot.start((ctx) => ctx.reply("Welcome to shady lyrics stan"));
 
-bot.command('whomadethis', (ctx) => ctx.reply('Abhinav'))
-bot.on('sticker', (ctx) => ctx.reply('❤️'))
+bot.command("whomadethis", (ctx) => ctx.reply("Abhinav"));
+bot.on("sticker", (ctx) => ctx.reply("❤️"));
 
+bot.command("lyrics", async function (ctx) {
+  let songName = ctx.message.text.substring(8);
+  const options = {
+    apiKey: process.env.GENIUS_API_KEY,
+    title: songName,
+    artist: "Eminem",
+    optimizeQuery: true,
+  };
+  let lyrics;
+  await getLyrics(options).then((llyrics) => {
+    lyrics = llyrics;
+  });
 
-
-
-bot.command('lyrics', async function(ctx){
-    let songName= ctx.message.text.substring(8);
-    const options = {
-        apiKey: process.env.GENIUS_API_KEY,
-        title: songName,
-        artist: 'Eminem',
-        optimizeQuery: true
-    };
-    let lyrics;
-    await getLyrics(options).then((llyrics) => {
-        lyrics=llyrics
-    });
-    
-    await ctx.reply(lyrics.substring(0,4095))
-    if(lyrics.length > 4095) ctx.reply(lyrics.substring(4095))
-})
+  await ctx.reply(lyrics.substring(0, 4095));
+  if (lyrics.length > 4095) ctx.reply(lyrics.substring(4095));
+});
 bot.launch();
